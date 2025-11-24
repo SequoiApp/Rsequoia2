895ad0cdcf1ce3f49d48ac976a3c37181ee233a3
Paul Carteron <p.carteron@racines.com>
2025-11-24 11:26:07 +0100

    rename seq_layer to seq_layers and utils-layer to utils-layers

---
+++ R/normalize.R
@@ -0,0 +1,2 @@
+cfg_path <- system.file("config/seq_fields.yaml", package = "Rsequoia2")
+cfg <- yaml::read_yaml(cfg_path)
--- R/utils-layers.R
+++ R/utils-layers.R
@@ -29,17 +29,17 @@ get_id <- function(dirname = ".", verbose = FALSE) {
 #'
 #' Builds the expected output filename for a given layer key by combining:
 #' (1) the forest ID extracted from the local *_matrice.xlsx file, and
-#' (2) the corresponding layer definition stored in `inst/config/seq_layer.yaml`.
+#' (2) the corresponding layer definition stored in `inst/config/seq_layers.yaml`.
 #'
 #' @param key `character` Name of a layer key to match against the entries
-#' defined in `inst/config/seq_layer.yaml`. (see *Details* for partial matching).
+#' defined in `inst/config/seq_layers.yaml`. (see *Details* for partial matching).
 #' @param dirname `character` Directory where the matrice file is located.
 #' Defaults to the current working directory.
 #' @param verbose `logical` If `TRUE`, display messages.
 #'
 #' @details
 #' The function resolves the input `key` using **partial matching** against
-#' keys defined in `inst/config/seq_layer.yaml`.
+#' keys defined in `inst/config/seq_layers.yaml`.
 #'
 #' If exactly **one** entry matches, it is selected : for example,
 #' `key = "znieff1"` can be used to match `v.inpn.znieff1.poly` because only
@@ -48,13 +48,13 @@ get_id <- function(dirname = ".", verbose = FALSE) {
 #' If **multiple** entries match, the user is shown the ambiguous options and
 #' must provide a more specific key.
 #'
-#' Note: **`seq_layer.yaml` is part of the package and should not be modified.**
+#' Note: **`seq_layers.yaml` is part of the package and should not be modified.**
 #'
 #' @return Invisibly returns a single forest ID (character scalar).
 #'
 get_path <- function(key, dirname = ".", verbose = FALSE){
 
-  cfg_path <- system.file("config/seq_layer.yaml", package = "Rsequoia2")
+  cfg_path <- system.file("config/seq_layers.yaml", package = "Rsequoia2")
   cfg <- yaml::read_yaml(cfg_path)
 
   all_key <- names(cfg)
@@ -73,7 +73,7 @@ get_path <- function(key, dirname = ".", verbose = FALSE){
     red_warn <- cli::combine_ansi_styles("red", "bold")
     cli::cli_abort(c(
       "x" = "{.arg key} {.val {key}} does not exist.",
-      "i" = "Valid keys are defined in {.path inst/config/seq_layer.yaml}.",
+      "i" = "Valid keys are defined in {.path inst/config/seq_layers.yaml}.",
       "!" = red_warn("This file is part of the package and must not be modified.")
     ))
   }
