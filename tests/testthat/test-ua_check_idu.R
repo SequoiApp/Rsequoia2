b54e71c77259e224fa17dcf197671963e8d61d66
Paul Carteron <p.carteron@racines.com>
2025-11-25 12:28:26 +0100

    simplify test for ua_check_idu() leveraging helpers

--- tests/testthat/test-ua_check_idu.R
+++ tests/testthat/test-ua_check_idu.R
@@ -1,96 +1,37 @@
-test_that("ua_check_idu_with_parca returns TRUE when all IDU match", {
-  skip_if_not_installed("sf")
-  library(sf)
+test_that("ua_check_idu returns TRUE when all IDU match", {
 
-  idu <- seq_field("idu")$name
-
-  ua <- st_sf(
-    setNames(list(1:3), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)), st_point(c(2,2))
-    )
-  )
-
-  parca <- st_sf(
-    setNames(list(1:3), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)), st_point(c(2,2))
-    )
-  )
+  parca <- fake_parca(numero = c("0001", "0002"))
+  ua <- seq_normalize(parca, "ua")
 
   expect_true(ua_check_idu(ua, parca, verbose = FALSE))
 })
 
-test_that("ua_check_idu_with_parca returns FALSE when IDU are missing", {
-  skip_if_not_installed("sf")
-  library(sf)
+test_that("ua_check_idu print message when verbose = TRUE", {
 
-  idu <- seq_field("idu")$name
+  parca <- fake_parca(numero = c("0001", "0002"))
+  ua <- seq_normalize(parca, "ua")
 
-  ua <- st_sf(
-    setNames(list(1:3), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)), st_point(c(2,2))
-    )
+  expect_message(
+    ua_check_idu(ua, parca, verbose = TRUE),
+    "All cadastral IDUs from"
   )
-
-  parca <- st_sf(
-    setNames(list(c(1,2,3,4)), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)),
-      st_point(c(2,2)), st_point(c(3,3))
-    )
-  )
-
-  expect_false(ua_check_idu(ua, parca, verbose = FALSE))
 })
 
-test_that("ua_check_idu emits CLI warning when verbose = TRUE", {
-  skip_if_not_installed("sf")
-  library(sf)
+test_that("ua_check_idu returns FALSE when IDU are missing", {
 
-  idu <- seq_field("idu")$name
+  parca <- fake_parca(1, 1, 1, 1)
+  ua <- seq_normalize(fake_parca(2, 2, 2, 2), "ua")
 
-  ua <- st_sf(
-    setNames(list(1:2), idu),
-    geometry = st_sfc(st_point(c(0,0)), st_point(c(1,1)))
-  )
-
-  parca <- st_sf(
-    setNames(list(c(1,2,3)), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)), st_point(c(2,2))
-    )
-  )
-
-  msg <- testthat::capture_messages(
-    res <- ua_check_idu(ua, parca, verbose = TRUE)
-  )
-
+  expect_warning(res <- ua_check_idu(ua, parca), "1111")
   expect_false(res)
-  expect_true(any(grepl("missing", msg, ignore.case = TRUE)))
 })
 
-test_that("ua_check_idu_with_parca detects multiple missing IDU", {
-  skip_if_not_installed("sf")
-  library(sf)
 
-  idu <- seq_field("idu")$name
+test_that("ua_check_idu detects multiple missing IDU", {
 
-  ua <- st_sf(
-    setNames(list(1:2), idu),
-    geometry = st_sfc(st_point(c(0,0)), st_point(c(1,1)))
-  )
+  parca <- fake_parca(1:3, 1:3, 1:3, 1:3)
+  ua <- seq_normalize(fake_parca(3:5, 3:5, 3:5, 3:5), "ua")
+  expect_warning(res <- ua_check_idu(ua, parca), "1111.*2222")
+  expect_false(res)
 
-  parca <- st_sf(
-    setNames(list(c(1,2,3,4,5)), idu),
-    geometry = st_sfc(
-      st_point(c(0,0)), st_point(c(1,1)),
-      st_point(c(2,2)), st_point(c(3,3)), st_point(c(4,4))
-    )
-  )
-
-  out <- ua_check_idu(ua, parca, verbose = FALSE)
-
-  expect_false(out)
 })
