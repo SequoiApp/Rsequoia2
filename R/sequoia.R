2628043a9a7972cc049fa8939baaadd23461eba9
Paul Carteron <p.carteron@racines.com>
2026-01-15 13:50:31 +0100

    polishing documentation 2

--- R/sequoia.R
+++ R/sequoia.R
@@ -5,7 +5,6 @@
 #'
 #' @param path `character` Optional. Path to the project folder. If not
 #' provided, the user will be prompted to enter it interactively.
-#' @param verbose `logical` If `TRUE`, display messages.
 #' @param overwrite `logical` If `TRUE`, file is overwritten.
 #'
 #' @details
