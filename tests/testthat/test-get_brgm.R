be01f52651634024af85a7585211eb5de97f35e9
Paul Carteron <p.carteron@racines.com>
2026-02-17 20:18:31 +0100

    Continuing mocking get_cog()

--- tests/testthat/test-get_brgm.R
+++ tests/testthat/test-get_brgm.R
@@ -34,7 +34,7 @@ test_that("get_brgm() works for one dep", {
     download_brgm = function(dep, ...) fake_brgm_zip(dep, cache = cache)
   )
 
-  brgm <- get_brgm(deps = dep, source = "bdcharm50", cache = cache, verbose = FALSE)
+  brgm <- get_brgm(deps = 29, source = "bdcharm50", cache = cache, verbose = FALSE)
 
   expect_s3_class(brgm, "sf")
   expect_shape(brgm, dim = c(1, 2))
