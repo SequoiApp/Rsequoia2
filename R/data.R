98ec74f713c5f231c01673bc7665acd29ecdebe4
mucau <matthieu.chevereau@hotmail.fr>
2026-05-25 10:39:19 +0200

    Ajout des couches BRIN

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' @title BRIN *PARCA* layer
+#'
+#' @description
+#' An example of *PARCA* layer for the Brin forest.
+#'
+#' @format An `sf` object with 114 obs and 18 colums.
+"brin_parca"
+
+#' @title BRIN *UA* layer
+#'
+#' @description
+#' An example of *UA* layer for the Brin forest.
+#' The information contained therein is entirely imaginary.
+#' It does not reflect the description or management of the forest at all.
+#'
+#' @format An `sf` object with 215 obs and 45 colums.
+"brin_ua"
